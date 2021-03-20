7e4dd9ac7dc6358fe2b306be2f4766001cba65cf
Sai Aasrith <sai.aasrith@gmail.com>
2021-03-20 15:44:19 +0530

    My initial doom emacs config

---
+++ config.el
@@ -0,0 +1,84 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Source Code Pro" :size 18)
+       doom-variable-pitch-font (font-spec :family "open sans" :size 20))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Aasrith/Dropbox/org/")
+
+;; org-roam config
+(after! org-roam
+  :init
+  (setq org-roam-index-file "~/Aasrith/Dropbox/org/roam/20210302202810-index.org")
+        (map! :leader
+            :prefix "r"
+            :desc "org-roam" "l" #'org-roam
+            :desc "org-roam-insert" "i" #'org-roam-insert
+            :desc "org-roam-switch-to-buffer" "b" #'org-roam-switch-to-buffer
+            :desc "org-roam-find-file" "f" #'org-roam-find-file
+            :desc "org-roam-show-graph" "g" #'org-roam-show-graph
+            :desc "org-roam-find-index" "I" #'org-roam-find-index))
+
+;; org-journal the DOOM way
+(use-package org-journal
+  :init
+  (setq org-journal-date-prefix "#+TITLE: "
+        org-journal-file-format "%Y%m%d.org"
+        org-journal-date-format "%A, %d %B %Y")
+  :config
+  (map! :leader
+        :prefix "j"
+        :desc "org-journal-new-entry" "j" #'org-journal-new-entry
+        :desc "org-journal-current-file" "t" #'org-journal-open-current-journal-file
+        :desc "org-journal-display-entry" "d" #'org-journal-display-entry
+        :desc "org-agenda" "a" #'org-agenda)
+  )
+
+(setq org-journal-enable-agenda-integration t)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
