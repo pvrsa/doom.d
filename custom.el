3793605deeaccf3fc0fef2113857191f447f2d47
Sai Aasrith <sai.aasrith@gmail.com>
2021-05-21 19:58:53 +0530

    Make org heading weights non-bold

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/sapottur/Aasrith/Dropbox/org/todo.org" "/Users/sapottur/Aasrith/Dropbox/org/journal/20210521.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:height 1.3))))
+ '(org-level-1 ((t (:inherit outline-1 :weight regular :height 1.4))))
+ '(org-level-2 ((t (:inherit outline-2 :weight regular :height 1.15))))
+ '(org-level-3 ((t (:inherit outline-3 :weight regular :height 1.12))))
+ '(org-level-4 ((t (:inherit outline-4 :weight regular :height 1.09))))
+ '(org-level-5 ((t (:inherit outline-5 :weight regular :height 1.06)))))
